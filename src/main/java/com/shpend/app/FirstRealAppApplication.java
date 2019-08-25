fd35299777096d91bffed603f88251018c793445
shpendpalushi <palushishpend@gmail.com>
2019-08-25 12:23:33 +0200

    New again

---
+++ src/main/java/com/shpend/app/FirstRealAppApplication.java
@@ -0,0 +1,13 @@
+package com.shpend.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRealAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstRealAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shpend/app/FirstRealAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shpend.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstRealAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
