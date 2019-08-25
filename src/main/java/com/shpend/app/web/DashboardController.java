6ca1d9a2d46a3a09342255b0ddae5f22ff429209
shpendpalushi <palushishpend@gmail.com>
2019-08-25 23:11:20 +0200

    Creating a login page

--- src/main/java/com/shpend/app/web/DashboardController.java
+++ src/main/java/com/shpend/app/web/DashboardController.java
@@ -1,4 +1,4 @@
-package com.shpend.app;
+package com.shpend.app.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ src/main/java/com/shpend/app/web/LoginController.java
@@ -0,0 +1,15 @@
+package com.shpend.app.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+	//@RequestMapping(value="/login",method=RequestMethod.GET)
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+}
