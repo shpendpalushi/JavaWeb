3a123ab4fc7e9b9d7497feae926325fa5ac344e7
shpendpalushi <palushishpend@gmail.com>
2019-08-25 19:32:23 +0200

    Adding the first controller eleminating the tests

---
+++ src/main/java/com/shpend/app/DashboardController.java
@@ -0,0 +1,15 @@
+package com.shpend.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class DashboardController {
+	
+	@RequestMapping(value="/" , method=RequestMethod.GET)
+	public String rootView() {
+		return "index";
+	}
+
+}
--- src/main/java/com/shpend/app/FirstRealAppApplication.java
+++ src/main/java/com/shpend/app/FirstRealAppApplication.java
@@ -1,12 +1,8 @@
 package com.shpend.app;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.shpend.app.domain.User;
-import com.shpend.app.repository.UserRepositoryGo;
 
 @SpringBootApplication
 public class FirstRealAppApplication implements CommandLineRunner {
@@ -14,14 +10,10 @@ public class FirstRealAppApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(FirstRealAppApplication.class, args);
 	}
-	
-	@Autowired
-	UserRepositoryGo repo;
 
 	@Override
 	public void run(String... args) throws Exception {
 		
-		repo.insertUser(new User(10001L, "shpendpalushi","boss", "shpend"));
 		
 	}
 
